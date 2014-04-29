94b2d0e97e50e63d46dcbae90bd098b21850d57a
EddyEND <eddy.end@gmail.com>
2014-04-29 23:43:09 +0200

    Przeniosłem navbar do szablon.master Dorzuciłem kod do zmiany navbar jak będzie zalogowany poprawka css plik do obsługi sesji

--- Default.aspx
+++ Default.aspx
@@ -1,11 +1,20 @@
 ﻿<%@ Page Title="Stron główna" Language="C#" MasterPageFile="~/Szablon.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
 
-<asp:Content ContentPlaceholderID="head" runat="server">
-
+<asp:Content ContentPlaceholderID="head" runat="server"></asp:Content>
+<asp:Content ContentPlaceholderID="Navbar" runat="server">
+    <div id="Navbar-Kalendarz">
+        <a href="#">Kalendarz</a>
+    </div>
+    <div id="Navbar-Rejestracja">
+        <a href="#">Rejestracja</a>
+    </div>
+    <div id="Navbar-Zaloguj">
+        <a href="Default.aspx?action=login">Zaloguj</a>
+    </div>
 </asp:Content>
 <asp:Content ContentPlaceholderID="MainContent" runat="server">
     <div id="Tresc">
-        <div></div>
+        <div><%=Application["i"] %></div><%-- wyświetlanie zmiennych --%>
     </div>
     <div id="Informacje-Full">
         <div>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,6 +9,23 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        
+        if (Request.QueryString["action"] == "login"){ // jakis inny warunek, ale tak się będzie zmieniac navbar dla zalogowanego
+            ContentPlaceHolder c = Page.Master.FindControl("Navbar") as ContentPlaceHolder;
+            if (c != null){
+                LiteralControl l = new LiteralControl();
+                l.Text =   @"<div id='Navbar-Kalendarz'>
+                                <a href='#'>Kalendarz</a>
+                            </div>
+                            <div id='Navbar-Rejestracja'>
+                                <a href='#'>Rejestracja</a>
+                            </div>
+                            <div id='Navbar-Zaloguj'>
+                                <a href='Default.aspx?action=logout'>Wyloguj</a>
+                            </div>";
+                c.Controls.Clear();
+                c.Controls.Add(l);
+            }
+        }
     }
 }
